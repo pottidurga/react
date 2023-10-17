053bf0c353400f369dbc2538814658d7d5727a61
pottidurga <145979252+pottidurga@users.noreply.github.com>
2023-10-17 16:00:22 +0530

    app

---
+++ App.js
@@ -0,0 +1,4 @@
+var parent=[React.createElement("h1",{},"welcometo java script"),React.createElement("h2",{},(React.createElement("h3",{},"react elements usig cdn links")))]
+var htmlRoot=document.getElementById("root")
+var rootReact=ReactDOM.createRoot(htmlRoot)
+rootReact.render(parent)
